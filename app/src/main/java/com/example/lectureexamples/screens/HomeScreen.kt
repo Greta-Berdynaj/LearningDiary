71f9673ed7fcb8178378472f3970f774ea4ce927
Greta-Berdynaj <greta.berdynaj@stud.fh-campuswien.ac.at>
2023-03-26 23:52:56 +0200

    Done

--- app/src/main/java/com/example/lectureexamples/screens/HomeScreen.kt
+++ app/src/main/java/com/example/lectureexamples/screens/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.lectureexamples.screens
 
 import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -11,8 +12,7 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,6 +35,7 @@ fun HomeScreen(navController: NavController) {
         color = MaterialTheme.colors.background
     ) {
         Column {
+            AppTopBar(navController)
             Greeting()
             Text(
                 style = MaterialTheme.typography.h6,
@@ -69,46 +70,82 @@ fun MyList(navController: NavController = rememberNavController(),
 
 @Composable
 fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}) {
-    Card(modifier = Modifier
-        .fillMaxWidth()
-        .padding(5.dp)
-        .clickable { onItemClick(movie.id) },
+    var showMoreInfo by remember {
+        mutableStateOf(false)
+    }
+    // to display the movie info
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+            .clickable { onItemClick(movie.id) },
         shape = RoundedCornerShape(corner = CornerSize(15.dp)),
         elevation = 5.dp
     ) {
+        // to hold the contents of the card
         Column {
-            Box(modifier = Modifier
-                .height(150.dp)
-                .fillMaxWidth()
+            // Box composable display the movie poster
+            Box(
+                modifier = Modifier
+                    .height(150.dp)
+                    .fillMaxWidth()
             ) {
+                // Image composable to display the movie poster
                 Image(
-                    painter = painterResource(id = R.drawable.avatar2),
+                    painter = painterResource(id = R.drawable.avatar2), // load the poster image
                     contentDescription = "Movie Poster",
                     contentScale = ContentScale.Crop
                 )
 
-                Box(modifier = Modifier
-                    .fillMaxSize()
-                    .padding(10.dp),
+                // Box composable to display the "Add to favorites" icon
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(10.dp),
                     contentAlignment = Alignment.TopEnd
-                ){
+                ) {
+                    // Icon composable to display the "Add to favorites" icon
                     Icon(
                         tint = MaterialTheme.colors.secondary,
                         imageVector = Icons.Default.FavoriteBorder,
-                        contentDescription = "Add to favorites")
+                        contentDescription = "Add to favorites"
+                    )
+                }
+            }
+            // To display the movie title and expand/collapse icon
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                // display the movie title
+                Text(movie.title, style = MaterialTheme.typography.h6)
+                // to display the expand/collapse icon
+                Icon(
+                    // choose the appropriate icon based on the state of showMoreInfo
+                    imageVector = if (showMoreInfo) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                    contentDescription = "arrowUp",
+                    // toggle the state of showMoreInfo when the icon is clicked
+                    modifier = Modifier.clickable { showMoreInfo = !showMoreInfo })
+            }
+            // display the additional movie info
+            Column(modifier = Modifier.fillMaxWidth()) {
+                // to animate the visibility of the additional movie info
+                AnimatedVisibility(visible = showMoreInfo) {
+                    Column {
+                        Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.caption)
+                        Text(text = "Released: ${movie.year}", style = MaterialTheme.typography.caption)
+                        Text(text = "Genre: ${movie.genre}", style = MaterialTheme.typography.caption)
+                        Text(text = "Actors: ${movie.actors}", style = MaterialTheme.typography.caption)
+                        Text(text = "Rating: ${movie.rating}", style = MaterialTheme.typography.caption)
+                        Text(text = "Plot: ${movie.plot}", style = MaterialTheme.typography.caption)
+
+                    }
+
                 }
             }
 
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(5.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(movie.title, style = MaterialTheme.typography.h6)
-                Icon(
-                    imageVector = Icons.Default.KeyboardArrowUp,
-                    contentDescription = "Show details")
-            }
         }
     }
 }
@@ -179,3 +216,37 @@ fun Greeting() {
          */
     }
 }
+@Composable
+fun AppTopBar(navController: NavController) {
+
+    var showMenu by remember {
+        mutableStateOf(false)
+    }
+
+    TopAppBar(
+        title = { Text(text = "Movies") },
+        actions = {
+
+            // IconButton to open the menu
+            IconButton(onClick = {
+                showMenu = !showMenu
+            }) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = "Open Options"
+                )
+            }
+            // drop down menu
+            DropdownMenu(
+                modifier = Modifier.width(width = 150.dp),
+                expanded = showMenu,
+                onDismissRequest = { showMenu=false}) {
+                // Menu item to display user's favorites
+                DropdownMenuItem(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Filled.Favorite, contentDescription = "Favorite")
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(text = "Favorites")
+                }
+
+            }
+        })}
